f9cd6655386fe950019da47764378b4c1e83b29c
zebamahfuz <zebamahfuz@gmail.com>
2023-12-11 20:41:25 +0000

    ini

--- HospitalManagementSystem/settings.py
+++ HospitalManagementSystem/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = '/sitehandler/static/'
 #STATIC_ROOT = BASE_DIR / 'productionfiles'
 
